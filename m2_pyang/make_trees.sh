4536614c1b70f0d7f23918d391abdcedbb76e735
Nick <njrusmc@gmail.com>
2023-06-12 06:40:17 -0400

    yang trees

--- m2_pyang/make_trees.sh
+++ m2_pyang/make_trees.sh
@@ -5,18 +5,18 @@ pyang_path="yang/vendor/cisco/xe/1741"
 
 echo "Producing flow containers to augment native"
 pyang --format tree --path $pyang_path \
-  --output data_ref/xe_flow.txt \
+  --output data_ref/xe_flow_tree.txt \
   $pyang_path/Cisco-IOS-XE-flow.yang
 
 # Option 1: Target interfaces, view source groupings
 echo "Producing interface groupings"
 pyang --format tree --path $pyang_path \
-  --output data_ref/xe_intf.txt --tree-print-groupings \
+  --output data_ref/xe_intf_tree.txt --tree-print-groupings \
   $pyang_path/Cisco-IOS-XE-interfaces.yang
 
 # Option 2: Target native, filter interfaces from hierarchy
 echo "Producing interface containers from native"
 pyang --format tree --path $pyang_path \
-  --output data_ref/xe_native.txt \
+  --output data_ref/xe_native_subtree.txt \
   --tree-path /native/interface/GigabitEthernet \
   $pyang_path/Cisco-IOS-XE-native.yang
